f131cd66c6fcdea67d2be6ecbbea39060de10de6
Nikko Nabil Valerian <nikko.nabil@gmail.com>
2026-07-20 18:31:33 +0700

    ClientID Added

--- js/config.js
+++ js/config.js
@@ -6,7 +6,7 @@
 // JANGAN PERNAH taruh "Client Secret" di file ini atau di kode frontend mana pun.
 
 const CONFIG = {
-  CLIENT_ID: "GANTI_DENGAN_CLIENT_ID_ANDA.apps.googleusercontent.com",
+  CLIENT_ID: "910024218137-bq996q3cq0c6qc29s242mnjmj5cs9c9k.apps.googleusercontent.com",
 
   // Scope paling minim: hanya baca struktur form, tidak bisa edit apa pun.
   SCOPES: "https://www.googleapis.com/auth/forms.body.readonly",
